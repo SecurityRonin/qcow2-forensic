4ddce9a2200e0d57e96d915fed44cfcdd9d90ba6
Albert Hui <albert@securityronin.com>
2026-05-22 15:16:11 +0800

    chore: add .gitignore; untrack target/

--- target/debug/deps/qcow2-e2fbb6898d8fcf3b.d
+++
@@ -1,8 +0,0 @@
-/Users/4n6h4x0r/src/qcow2/target/debug/deps/qcow2-e2fbb6898d8fcf3b.d: qcow2/src/lib.rs qcow2/src/error.rs qcow2/src/header.rs qcow2/src/testutil.rs
-
-/Users/4n6h4x0r/src/qcow2/target/debug/deps/qcow2-e2fbb6898d8fcf3b: qcow2/src/lib.rs qcow2/src/error.rs qcow2/src/header.rs qcow2/src/testutil.rs
-
-qcow2/src/lib.rs:
-qcow2/src/error.rs:
-qcow2/src/header.rs:
-qcow2/src/testutil.rs:
